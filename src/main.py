d6b0f9aaed49434d28b25a9879083de5c47fc6ab
primboi <81822069+primitt@users.noreply.github.com>
2022-09-24 12:02:10 -0700

    Add shit

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from distutils.command.config import config
-from flask import Flask, render_template, request, redirect, url_for, flash
-
+from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
+#import idom
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
+@app.route('/<string:page_name>')
+def sendfile(page_name):
+    return send_from_directory('static', page_name)
 if __name__ == "__main__":
     app.run(debug=True)
