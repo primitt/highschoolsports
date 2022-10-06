1fd27047f3bdd30c7e53bdadc98763820af632f4
primboi <81822069+primitt@users.noreply.github.com>
2022-10-06 13:08:52 -0700

    Added many new things, game is almost created!!!

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from distutils.command.config import config
 from sre_constants import SUCCESS
+from tkinter import E
 from flask import Flask, make_response, render_template, request, redirect, url_for, flash, send_from_directory
 import pymongo 
 import os
@@ -17,6 +18,8 @@ def hash_password(password):
     return hashedpassword
 def passwordValidate(password, hashedpassword):
     print(password, hashedpassword)
+    if hashlib.sha256(password.encode('utf-8')).hexdigest() == hashedpassword:
+        return True
     if password == hashedpassword:
         return True
     else:
@@ -25,6 +28,19 @@ def fastLogin(username, hpassword):
     print(username, hpassword)
     try:
         data = list(accounts.find({"username":username}))
+        if data == []:
+            return False
+        else:
+            if passwordValidate(hpassword, data[0]["password"]):
+                return True
+            else:
+                return False
+    except:
+        return False
+def adminFastLogin(username, hpassword):
+    print(username, hpassword)
+    try:
+        data = list(admindb.find({"username":username}))
         if data == []:
             return False
         else:
@@ -47,10 +63,20 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     try:
-        loggedin = fastLogin(request.cookies.get("session").split("/")[0], request.cookies.get("session").split("/")[1])
-        print(loggedin)
+        log = fastLogin(request.cookies.get("session").split("/")[0], request.cookies.get("session").split("/")[1])
+        if log:
+            loggedin = "SESSIONTRUE"
+        else:
+            loggedin = "SESSIONFALSE"
     except:
-        loggedin = False
+        try:
+            log = adminFastLogin(request.cookies.get("adminsession").split("/")[0], request.cookies.get("adminsession").split("/")[1])
+            if log:
+                loggedin = "ADMINTRUE"
+            else:
+                loggedin = "ADMINFALSE"
+        except:
+            loggedin = "FALSE"
     return render_template('index.html', loggedin=loggedin)
 @app.route('/search', methods=['POST'])
 def search():
@@ -163,12 +189,69 @@ def adminlogin():
 # TODO: Make "verified" email a thing
 # TODO: Make a system to add score and such
 # TODO: Make admin panel
+@app.route('/admin/creategame', methods=['GET', 'POST'])
+def creategame():
+    if request.method == "POST":
+        if request.cookies.get("adminsession"):
+            if adminFastLogin(request.cookies.get("adminsession").split("/")[0], request.cookies.get("adminsession").split("/")[1]):
+                gamename = request.form['name']
+                date = request.form['date']
+                time = request.form['time']
+                location = request.form['location']
+                opponent = request.form['opponent']
+                sport = request.form['sport']
+                if gamename == "":
+                    return render_template('creategame.html', error="Game name cannot be blank")
+                if date == "":
+                    return render_template('creategame.html', error="Date cannot be blank")
+                if time == "":
+                    return render_template('creategame.html', error="Time cannot be blank")
+                if location == "":
+                    return render_template('creategame.html', error="Location cannot be blank")
+                if opponent == "":
+                    return render_template('creategame.html', error="Opponent cannot be blank")
+                if sport == "":
+                    return render_template('creategame.html', error="Sport cannot be blank")
+                # get schools from admindb
+                admin = list(admindb.find({"username":request.cookies.get("adminsession").split("/")[0]}))
+                if admin == []:
+                    return redirect(url_for('adminlogin'))
+                else:
+                    school = list(schools.find({"id":int(admin[0]['schools'][0])}))[0]["teamname"] # FIXME: This needs to be automatic school finder in the future!, PLEASE ADVISE
+                    col.insert_one({"gamename":gamename, "date":date, "time":time, "home":school, "location":location, "opponent":opponent, "sport":sport, "score":{"home":0, "away":0}})
+                    return redirect(url_for('admin'))
+    else:
+        if request.cookies.get("adminsession"):
+            if adminFastLogin(request.cookies.get("adminsession").split("/")[0], request.cookies.get("adminsession").split("/")[1]):
+                return render_template('creategame.html')
+            else:
+                return redirect(url_for('adminlogin'))
+        else:
+            return redirect(url_for('adminlogin'))
 @app.route('/admin')
 def admin():
     if request.cookies.get("adminsession"):
-        return render_template('admin.html')
+        if adminFastLogin(request.cookies.get("adminsession").split("/")[0], request.cookies.get("adminsession").split("/")[1]):
+            # get all games
+            games = list(col.find())
+            if games == []:
+                return render_template('admin.html', games="none")
+            return render_template('admin.html', games=games)
+        else:
+            return redirect(url_for('adminlogin'))
     else:
-        return redirect(url_for('index'))
+        return redirect(url_for('adminlogin'))
+
+@app.route('/logout')
+def logout():
+    resp = make_response(redirect(url_for('index')))
+    resp.set_cookie('session', '', expires=0)
+    return resp
+@app.route('/admin/logout')
+def adminlogout():
+    resp = make_response(redirect(url_for('index')))
+    resp.set_cookie('adminsession', '', expires=0)
+    return resp
 @app.route('/<string:page_name>')
 def sendfile(page_name):
     return send_from_directory('static', page_name)
