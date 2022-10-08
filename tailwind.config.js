79adb6b9a03419143bb1ebdba4368c4c077abc73
primboi <81822069+primitt@users.noreply.github.com>
2022-10-07 19:12:18 -0700

    Commit new files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// This is only used for vscode intellisense
+
+module.exports = {
+    content: ["./src/templates/**/*.html"],
+    theme: {
+      extend: {},
+    },
+    plugins: [require("daisyui")],
+}
